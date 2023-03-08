6dcb2021a3a3d58db37656f33a575a72f6ddbedb
Vitaly <wiatly@mail.ru>
2023-03-08 14:48:36 +0300

    Seminar_6 added

--- Seminar_6/Task_30.py
+++ Seminar_6/Task_30.py
@@ -2,6 +2,6 @@ first_el = int(input("Введите первый элемент : "))
 count_el = int(input("Введите количество элементов : "))
 div_el = int(input("Разность : "))
 
-new_list = [first_el + (el-first_el) / div_el for el in range(first_el, first_el + count_el)]
+new_list = [first_el + el* div_el for el in range(count_el)]
 
 print(new_list)
